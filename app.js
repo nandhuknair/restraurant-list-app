2359274515ac883398eebe33949e90f100282b85
Nandhu <148442024+nandhuknair@users.noreply.github.com>
2024-05-28 22:38:58 +0530

    parent and child div using react CDN

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
+const parent = React.createElement("div",{id:'parent'},
+    React.createElement("div",{id:'child'},
+       [React.createElement("h1",{},"I'm h1 tag"),
+        React.createElement("h2",{},"i'm h2 tag")
+       ]
+    )
+);
+
 const heading = React.createElement("h1",
 {id : "heading" , className: "heading"},
 "Hello World From React!");
 
 console.log(heading)
+console.log(parent)
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(heading);
+root.render(parent);
