0b23549be7a00201d799b32bccd9b7b9d5592466
Nandhu <148442024+nandhuknair@users.noreply.github.com>
2024-06-05 11:56:21 +0530

    created filter using useState

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,13 +1,20 @@
 import RestaurantCard from "./RestaurantCard";
-import { resList } from "../utils/mockData";
+import { resList as initialList} from "../utils/mockData";
+import { useState } from "react";
+
 const Body =()=> {
+   const [resList,setResList] = useState(initialList)
     return (
         <div className="body">
             <div className="search"> Search</div>
+           <button onClick={()=> {
+            filteredResList = resList.filter(res=> res.info.avgRating > 4)
+            setResList(filteredResList)
+           }}>
+            Top Rated Restaurant !
+           </button>
             <div className="res-container">
-              {
-                 resList.map(restaurant=> <RestaurantCard key={restaurant.info.id} resData = {restaurant}/>)
-              }
+             {resList.map(res=> <RestaurantCard key = {res.info.id} resData={res}/>)}
             </div>
         </div>
     )
